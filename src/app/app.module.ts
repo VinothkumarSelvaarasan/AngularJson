5483fd7f7c1f9bceb538eb5b0cdad851d4a75f2a
VinothkumarSelvaarasan <vinothkumar.selvaarasan@gmail.com>
2024-07-30 17:03:42 +0530

    Angular Json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddVideosComponent } from './add-videos/add-videos.component';
+import { ViewVideosComponent } from './view-videos/view-videos.component';
+import { ViewVideoDetailsComponent } from './view-video-details/view-video-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddVideosComponent,
+    ViewVideosComponent,
+    ViewVideoDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
